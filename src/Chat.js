bb9f9ff6768b748b0ba6dc7ca43386d6de5d294a
Abhishek Mishra <mabhishek4156@gmai.com>
2022-04-12 12:49:09 +0530

    add footbar component

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,21 @@
 import { Avatar, IconButton } from '@material-ui/core';
-import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
+
+import userEvent from '@testing-library/user-event';
 import React, { useEffect, useState } from 'react';
 import './Chat.css';
 const Chat = () => {
+  const [input, setInput] = useState('');
   const [seed, setSeed] = useState('');
 
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log('you type <<<<<', input);
+  };
 
   return (
     <div className='chat'>
@@ -28,12 +36,25 @@ const Chat = () => {
             <MoreVert />
           </IconButton>
         </div>
+        9
       </div>
       <div className='chat__body'>
-        <p className='chat__message'>Hey Guys</p>
-        <span className='chat__name'>Abishek mishra</span>
+        <p className={`chat__message ${true && 'chat__reciever'}`}>
+          <span className='chat__name'>Abishek mishra</span>
+          Hey Guys
+          <span className='chat__timestamp'>5:40</span>
+        </p>
       </div>
-      <div className='chat__footer'></div>
+      <div className='chat__footer'>
+        <InsertEmoticon />
+        <form>
+          <input value={input} onChange={(e) => setInput(e.target.value)} type='text' placeholder='Type a massage' />
+          <button onClick={sendMessage} type='submit'>
+            Send a massage
+          </button>
+        </form>
+        <Mic />
+      </div>
     </div>
   );
 };
